08ed33d6c0769974fe5675f88cd31523ba5b2e52
Sean Philippi <sean.philippi@gmail.com>
2019-06-01 10:30:46 -0500

    added styling, images rendering

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -2,16 +2,28 @@ import React from 'react';
 
 const Beer = props => {
   const myStyles = {
-    // ! images not rendering
-    backgroundImage: "url(${props.beer.image_url})",
+    backgroundImage: `url(${props.beer.image_url})`,
+    height: '550px',
+    width: '120px',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   }
+  console.log('props', props.beer.image_url)
 
   return (
-    <div>
-      <div className="name">{props.beer.name}</div>
-      <div className="ph">ph: props.beer.ph</div>
-      <div className="tips">Brewer tips: </div>
-      <div className="image" style={myStyles}>
+    <div className="beer">
+      <div className="beer-info">
+        <div className="name">{props.beer.name}</div>
+        <div className="ph">ph: {props.beer.ph}</div>
+        <div className="tips">Brewers tips: {props.beer.brewers_tips}</div>
+      </div>
+      <div
+        className="image"
+        style={myStyles}>
       </div>
     </div>
   )
