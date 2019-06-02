462f0f0e7b8c7d1824137b44c1ee516be6ca4ec5
Sean Philippi <sean.philippi@gmail.com>
2019-06-02 16:44:42 -0500

    added site title

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,17 @@ class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        {
-          this.props.beers.map(beer => <Beer key={beer.id} beer={beer} checkLiked={this.props.checkLiked} />)
-        }
+        <div className="site-title">
+          BrewDog Back Catalogue
+        </div>
+        <div className="subheading">
+          Explore here
+        </div>
+        <div className="beers-container">
+          {
+            this.props.beers.map(beer => <Beer key={beer.id} beer={beer} checkLiked={this.props.checkLiked} />)
+          }
+        </div>
       </div>
     );
   }
